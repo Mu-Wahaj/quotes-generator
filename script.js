5c4c270d3bfcb1ccb0179d6a9758fea26ff56728
mwahajjutt123@gmail.comgit config --global user.email mwahajjutt123@gmail.com <mwahajjutt123@gmail.com>
2024-09-29 01:05:22 +0500

    upgrated

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const author = document.getElementById("auther");
 const newBtn = document.getElementById("newquote");
 const threadBtn = document.getElementById("post-thread");
 const api_Url = "https://programming-quotesapi.vercel.app/api/random";
-
 async function getQuote(url) {
   const response = await fetch(url);
   var data = await response.json();
@@ -11,11 +10,9 @@ async function getQuote(url) {
   author.innerHTML = data.author;
 }
 getQuote(api_Url);
-
 newBtn.addEventListener("click", function () {
   getQuote(api_Url);
 });
-
 function thread() {
   window.open(
     "https://threads.net/intent/post?text=" +
